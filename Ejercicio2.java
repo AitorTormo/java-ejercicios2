5be2d102a05581c92553ac98be3304f7429fb883
AitorTormo <aitorenova1988@gmail.com>
2022-02-10 00:31:04 +0100

    subiendo ejercicio2

---
+++ Ejercicio2.java
@@ -0,0 +1,30 @@
+package com.java;
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+
+        int precio1 = 10;
+        int precio2 = 34;
+        float precio3 = 87.95f;
+        float precio4 = 17.65f;
+
+        double precioIVA1 = precio1 + precio1 * 0.21;
+        double precioIVA2 = precio2 + precio2 * 0.21;
+        double precioIVA3 = precio3 + precio3 * 0.21;
+        double precioIVA4 = precio4 + precio4 * 0.21;
+
+        System.out.println(precioIVA1);
+        System.out.println(precioIVA2);
+        System.out.println(precioIVA3);
+        System.out.println(precioIVA4);
+
+    }
+
+}
+
+
+
+
+
+
